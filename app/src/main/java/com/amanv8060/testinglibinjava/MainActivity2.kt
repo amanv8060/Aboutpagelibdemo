8e0d5b566f0baedd25b0f89af3435de1bccff951
Aman Verma <verma1090aman@gmail.com>
2021-03-11 22:18:49 +0530

    Using 2.0.0

--- app/src/main/java/com/amanv8060/testinglibinjava/MainActivity2.kt
+++ app/src/main/java/com/amanv8060/testinglibinjava/MainActivity2.kt
@@ -2,13 +2,7 @@ package com.amanv8060.testinglibinjava
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.setContent
-import androidx.compose.ui.tooling.preview.Preview
-import com.amanv8060.testinglibinjava.ui.theme.TestingLibinJavaTheme
+import androidx.activity.compose.setContent
 import com.mx.aboutpage.AboutActivity
 import com.mx.aboutpage.AboutConfig
 
@@ -18,7 +12,7 @@ class MainActivity2 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         aboutConfig.appName = "Messengerx"
-        aboutConfig.version = "1.0.0"
+        aboutConfig.version = "2.0.0"
         aboutConfig.aboutLabelTitle = "About App"
         aboutConfig.packageName = applicationContext.packageName
         aboutConfig.shareMessage="Download this Awsome APP"
